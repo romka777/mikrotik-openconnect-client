f96e750af1865853891b2729ce2a82ddd59ab434
Roman Gogolev <ro-ro-ro@mail.ru>
2026-06-07 23:06:01 +0300

    support arm32

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine
 
-RUN set -xe && apk add --no-cache openconnect iptables && echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf 
+RUN set -xe && apk add --no-cache openconnect iptables iptables-legacy && echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf 
 
 COPY docker-entrypoint.sh /entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "$INTERFACE_NAME" ]; then
   INTERFACE_NAME="vpn0"
 fi
 
-iptables -t nat -A POSTROUTING -o $INTERFACE_NAME -j MASQUERADE
+iptables-legacy -t nat -A POSTROUTING -o $INTERFACE_NAME -j MASQUERADE
 
 exec openconnect $SERVER $OC_OPTIONS --user=$USERNAME --passwd-on-stdin --interface=$INTERFACE_NAME  <<EOF
 ${PASSWORD}
